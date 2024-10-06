263dbc3f9e7efba64b9d61cb0fc51250d08ca9a9
tech-gecko <w0nderzz2001@gmail.com>
2024-10-06 23:54:57 +0100

    Update task

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,13 +1,31 @@
-import math
+#!/usr/bin/python3
+"""This file houses a function that generates pascal triangle
+most likely, several functions with same purpose will be added
+then their speed and efficiency will be compared"""
+
 
 def pascal_triangle(n):
-    triangle = []
+    """This function returns a list of lists
+    that represent the pascla triangle of n"""
 
     if n <= 0:
-        return triangle
+        return []
+    if n == 1:
+        return [[1]]
 
-    else:
-        for row in range(n):
-            triangle.append([math.comb(row, index) for index in range(row + 1)])
+    current = [1]
+    triangle = [current]
+
+    for x in range(1, n):
+        prev = triangle[-1]
+        if len(prev) == 1:
+            current = [1, 1]
+        else:
+            current = [1]
+            for y in range(1, len(prev)):
+                current.append(prev[y-1] + prev[y])
+            current.append(1)
+
+        triangle.append(current)
 
     return triangle
